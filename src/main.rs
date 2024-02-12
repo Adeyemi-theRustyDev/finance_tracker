480cc00417ef4d98704608a941ea6e6a4835c3a4
Adeyemi-theRustyDev <abiadeabdulazeez@gmail.com>
2024-02-12 07:21:08 +0100

    Black bg

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,18 @@ fn main() {
 }
 
 slint::slint! {
+/*    component Title inherits Text {
+        width: 100px;
+        height: 40px;
+        text: "Finance Tracker";
+        color: white;
+    } */
+    
     export component Mainwindow inherits Window {
+        preferred_width: 650px;
+        preferred_height: 750px;
+        background: black;
+        // TitleText{}
         Text {
             text: "Hey there, it is I, Commandliner";
             color: green;
